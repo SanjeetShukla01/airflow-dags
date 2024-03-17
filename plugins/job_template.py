5cfd0122be1bd167fb7632b36d9ded26d82d6423
SanjeetShukla01 <sanjeet.shukla089@gmail.com>
2024-03-17 16:46:53 +0530

    adding docstring

--- plugins/job_template.py
+++ plugins/job_template.py
@@ -4,17 +4,17 @@ import logging
 
 
 class JobTemplate(metaclass=abc.ABCMeta):
-
     def __init__(self, params):
-        self.params =params
-
+        self.params = params
 
     @property
     @abc.abstractmethod
     def template(self):
+        """Abstract property representing the job template."""
         pass
 
     def generate_config(self):
+        """Generate configuration based on the job parameters."""
         params_copy = copy.copy(self.params)
         del params_copy['task_id']
         return {
@@ -22,9 +22,11 @@ class JobTemplate(metaclass=abc.ABCMeta):
         }
 
     @abc.abstractmethod
-    def generate_command(self, task_params:dict):
+    def generate_command(self, task_params: dict):
+        """Abstract method to generate the command based on task parameters."""
         pass
 
-    def validate_template_params(self, task_params:dict):
+    def validate_template_params(self, task_params: dict):
+        """Validate the task parameters against the template."""
         logging.info('No validation function was specified for this template --> no validation was done for the params for this task')
         pass
